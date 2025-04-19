d548009fcf191f86c6c8f78bbdd307ee6f571196
Błażej Pawluk <bpawluk05@gmail.com>
2025-04-19 14:08:23 +0200

    aisd/l/l3/4-2 test + charts

---
+++ sem4/aisd/lista3/aaa/chartGenerators/chartGenerator4-2.py
@@ -0,0 +1,88 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def load_data(path):
+    try:
+        data = np.genfromtxt(path, dtype=None, encoding='utf-8', names=True)
+        return data
+    except Exception as e:
+        print(f"Unable to load data from {path}: {e}")
+        return None
+
+def process_data(data):
+    results = {}
+    unique_labels = np.unique(data['label'])
+
+    for label in unique_labels:
+        mask_label = data['label'] == label
+        data_label = data[mask_label]
+
+        unique_ns = np.unique(data_label['n'])
+
+        avg_comparisons = []
+        avg_times = []
+        ns = []
+
+        for n_val in unique_ns:
+            mask_n = data_label['n'] == n_val
+            avg_comp = np.mean(data_label[mask_n]['comps'])
+            avg_time = np.mean(data_label[mask_n]['time'])
+            ns.append(n_val)
+            avg_comparisons.append(avg_comp)
+            avg_times.append(avg_time)
+
+        results[label] = {
+            'n': np.array(ns),
+            'avg_comparisons': np.array(avg_comparisons),
+            'avg_time': np.array(avg_times)
+        }
+    
+    return results
+
+def plot_results(results):
+    custom_labels = {
+        'ran': 'Random value from range [1, 2n-1]',
+        'beg': 'Value from the beginning of the array (index in [1, n/3])',
+        'mid': 'Value from the middle of the array (index in [n/3, 2n/3])',
+        'end': 'Value from the end of the array (index in [2n/3, n-1])',
+        'not': 'Random value from range [1, 2n-1], not in array',
+        'inA': 'Random value from array'
+	}
+
+    plt.figure(figsize=(10, 6))
+    for label, stats in sorted(results.items()):
+        plt.plot(stats['n'], stats['avg_comparisons'], marker='o', markersize=3, label=custom_labels.get(label, label))
+    plt.title('comps')
+    plt.xlabel('n')
+    plt.ylabel('average comps')
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    plt.savefig("charts/4-2_comps.png")
+    plt.close()
+
+    plt.figure(figsize=(10, 6))
+    for label, stats in sorted(results.items()):
+        plt.plot(stats['n'], stats['avg_time'], marker='o', markersize=3, label=custom_labels.get(label, label))
+    plt.title('time')
+    plt.xlabel('n')
+    plt.ylabel('average time')
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    plt.savefig("charts/4-2_time.png")
+    plt.close()
+
+def main():
+    filename = 'results/binSearchTest.txt'
+
+    data = load_data(filename)
+    if data is None:
+        return
+    
+    results = process_data(data)
+    plot_results(results)
+    print("Plots saved as '4-2_comps.png', '4-2_time.png'.")
+
+if __name__ == '__main__':
+    main()
---
+++ sem4/aisd/lista3/aaa/zadanie4-2.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <random>
+#include <fstream>
+#include <algorithm>
+#include <chrono>
+#include "lib/Algorithms.h"
+
+int main() {
+	std::random_device rd;
+	std::mt19937 gen(rd());
+
+	std::ofstream file("results/binSearchTest.txt");
+	file << "label n comps time\n";
+
+	BinSearch b;
+
+	for (int n = 1000; n <= 100000; n += 1000) {
+		std::cout << n << std::endl;
+
+		std::uniform_int_distribution<> dis(0, 2 * n - 1);
+		std::uniform_int_distribution<> disRan(0, 2 * n - 1);
+		std::uniform_int_distribution<> disBeg(0, n / 3);
+		std::uniform_int_distribution<> disMid(n / 3, 2 * n / 3);
+		std::uniform_int_distribution<> disEnd(2 * n / 3, n - 1);
+		std::uniform_int_distribution<> disNot(0, 2 * n - 1);
+		std::uniform_int_distribution<> disInA(0, n - 1);
+
+		for (int m = 0; m < 100; m++) {
+			int A[n];
+			for (int i = 0; i < n; i++) {
+				A[i] = dis(gen);
+			}
+			std::sort(A, A + n);
+
+			int ran = disRan(gen);
+			auto startTime = std::chrono::high_resolution_clock::now();
+			b.binSearch(A, n, ran);
+			auto endTime = std::chrono::high_resolution_clock::now();
+			auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - startTime).count();
+			file << "ran " << n << " " << b.c.getComps() << " " << elapsed << std::endl;
+
+			int beg = A[disBeg(gen)];
+			startTime = std::chrono::high_resolution_clock::now();
+			b.binSearch(A, n, beg);
+			endTime = std::chrono::high_resolution_clock::now();
+			elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - startTime).count();
+			file << "beg " << n << " " << b.c.getComps() << " " << elapsed << std::endl;
+			
+			int mid = A[disMid(gen)];
+			startTime = std::chrono::high_resolution_clock::now();
+			b.binSearch(A, n, mid);
+			endTime = std::chrono::high_resolution_clock::now();
+			elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - startTime).count();
+			file << "mid " << n << " " << b.c.getComps() << " " << elapsed << std::endl;
+			
+			int end = A[disEnd(gen)];
+			startTime = std::chrono::high_resolution_clock::now();
+			b.binSearch(A, n, end);
+			endTime = std::chrono::high_resolution_clock::now();
+			elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - startTime).count();
+			file << "end " << n << " " << b.c.getComps() << " " << elapsed << std::endl;
+			
+			int notA = disNot(gen);
+			while(b.binSearch(A, n, notA) != -1) {
+				notA = disNot(gen);
+			}
+			startTime = std::chrono::high_resolution_clock::now();
+			b.binSearch(A, n, notA);
+			endTime = std::chrono::high_resolution_clock::now();
+			elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - startTime).count();
+			file << "not " << n << " " << b.c.getComps() << " " << elapsed << std::endl;
+			
+			int inA = A[disInA(gen)];
+			startTime = std::chrono::high_resolution_clock::now();
+			b.binSearch(A, n, inA);
+			endTime = std::chrono::high_resolution_clock::now();
+			elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(endTime - startTime).count();
+			file << "inA " << n << " " << b.c.getComps() << " " << elapsed << std::endl;
+		}
+	}
+
+	file.close();
+
+	return 0;
+}
