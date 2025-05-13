f13aceb68814be0fe0e7b24edaf6c68aacfa7888
blazejpawluk <bpawluk05@gmail.com>
2025-05-13 10:51:24 +0200

    si/l2 przyklady

--- sem4/si/lista2/4x4.cpp
+++ sem4/si/lista2/4x4.cpp
@@ -129,47 +129,51 @@ int main() {
 	random_device rd;
 	mt19937 gen(rd());
 
+	// board = {12, 10, 14, 11, 9, 8, 6, 2, 0, 7, 15, 13, 4, 1, 5, 3}; uint8_t emptyIndex = 8;
+	// board = {4, 12, 15, 5, 14, 10, 11, 13, 0, 7, 2, 8, 6, 9, 1, 3}; uint8_t emptyIndex = 8;
+	// board = {7, 5, 8, 12, 1, 6, 9, 14, 10, 0, 2, 4, 3, 11, 13, 15}; uint8_t emptyIndex = 8;
+
 	// losowa permutacja
-	do {
-		shuffle(board.begin(), board.end() - 1, gen);
-	} while (!isSolvable(board));
-	uint64_t startingState = pack(board);
-	uint8_t emptyIndex = 15;
+	// do {
+	// 	shuffle(board.begin(), board.end() - 1, gen);
+	// } while (!isSolvable(board));
+	// uint8_t emptyIndex = 15;
 
 	// losowe przesunięcia
-	// int index = 15;
-	// uniform_int_distribution<> dis(1, 4);
-	// for (int i = 0; i < 20; i++) {
-	// 	int column = index % 4, row = index / 4;
+	int index = 15;
+	uniform_int_distribution<> dis(1, 4);
+	for (int i = 0; i < 20; i++) {
+		int column = index % 4, row = index / 4;
 
-	// 	bool legal = false;
-	// 	while (!legal) {
-	// 		int direction = dis(gen);
-	// 		switch (direction) {
-	// 			case 1: if (column - 1 >= 0) {
-	// 				legal = true;
-	// 				column--;
-	// 			} break;
-	// 			case 2: if (column + 1 < 4) {
-	// 				legal = true;
-	// 				column++;
-	// 			} break;
-	// 			case 3: if (row - 1 >= 0) {
-	// 				legal = true;
-	// 				row--;
-	// 			} break;
-	// 			case 4: if(row + 1 < 4) {
-	// 				legal = true;
-	// 				row++;
-	// 			} break;
-	// 		}
-	// 	}
-	// 	int newIndex = row * 4 + column;
-	// 	swap(board[index], board[newIndex]);
-	// 	index = newIndex;
-	// }
-	// uint64_t startingState = pack(board);
-	// uint8_t emptyIndex = (int)index;
+		bool legal = false;
+		while (!legal) {
+			int direction = dis(gen);
+			switch (direction) {
+				case 1: if (column - 1 >= 0) {
+					legal = true;
+					column--;
+				} break;
+				case 2: if (column + 1 < 4) {
+					legal = true;
+					column++;
+				} break;
+				case 3: if (row - 1 >= 0) {
+					legal = true;
+					row--;
+				} break;
+				case 4: if(row + 1 < 4) {
+					legal = true;
+					row++;
+				} break;
+			}
+		}
+		int newIndex = row * 4 + column;
+		swap(board[index], board[newIndex]);
+		index = newIndex;
+	}
+	uint8_t emptyIndex = (int)index;
+
+	uint64_t startingState = pack(board);
 
 	cout << "Stan poczatkowy: \n";
 	printBoard(board);
