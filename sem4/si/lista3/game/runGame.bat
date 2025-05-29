cdf27f4d23752160bc3bafb3f353b521c6c46aba
blazejpawluk <bpawluk05@gmail.com>
2025-05-29 20:30:55 +0200

    si/l/l3 skrypt na losowego

--- sem4/si/lista3/game/runGame.bat
+++ sem4/si/lista3/game/runGame.bat
@@ -6,8 +6,12 @@ start "Game Server" cmd /k ^
 
 REM 2) Klient
 start "Game Client" cmd /k ^
-  "cd /d %~dp0 && game\game_client.exe 127.0.0.1 4444 1 client"
+  "cd /d %~dp0 && game\game_client.exe 127.0.0.1 4444 2 client"
+
+@REM REM 2) Losowy bot
+@REM start "Random Bot Client" cmd /k ^
+@REM   "cd /d %~dp0 && game\game_random_bot.exe 127.0.0.1 4444 2 client"
 
 REM 3) Bot
 start "Bot Client" cmd /k ^
-  "cd /d %~dp0 && javac -d bin java\*.java && java -cp bin BotClient 127.0.0.1 4444 2 bot 5"
+  "cd /d %~dp0 && javac -d bin java\*.java && java -cp bin BotClient 127.0.0.1 4444 1 bot 5"
