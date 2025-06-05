a087ccf892b574d64a0e3f858550081a4bbef264
Błażej Pawluk <bpawluk05@gmail.com>
2025-06-05 10:44:09 +0200

    si/l3/minimax improvement

--- sem4/si/lista3/game/java/Bot.java
+++ sem4/si/lista3/game/java/Bot.java
@@ -1,166 +1,200 @@
-// import java.util.Random;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
 
-// class Move {
-// 	int score;
-// 	int depth;
-// 	private final static Random RAND = new Random();
-// 	public Move(int s, int d) {score = s; depth = d;}
+class Result {
+	int score;
+	int depth;
+	private final static Random RAND = new Random();
+	public Result(int s, int d) {score = s; depth = d;}
 
-// 	boolean isBetter(Move a, int previous) {
-// 		if (this.score > a.score) return true;
-// 		if (this.score < a.score) return false;
-// 		if (this.depth > a.depth) return this.score >= previous ? true : false;
-// 		if (this.depth < a.depth) return this.score >= previous ? false : true;
-// 		return RAND.nextBoolean();
-// 	}
-// }
+	boolean isBetter(Result a, int previous) {
+		if (this.score > a.score) return true;
+		if (this.score < a.score) return false;
+		if (this.depth > a.depth) return this.score >= previous ? true : false;
+		if (this.depth < a.depth) return this.score >= previous ? false : true;
+		return RAND.nextBoolean();
+	}
+}
 
-// public class Bot {
-// 	private static int previousScore;
+class Move {
+	int score;
+	int col, row;
+	public Move(int s, int c, int r) {score = s; col = c; row = r;}
+}
 
-// 	public static int move(int player, int depth) {
-// 		previousScore = evaluate(player);
-// 		Move bestScore = new Move(-1000000, depth+1);
-// 		int bestMove = -1;
+public class Bot {
+	private static int previousScore;
 
-// 		for (int i = 0; i < 5; i++) {
-// 			for (int j = 0; j < 5; j++) {
-// 				if (Board.board[i][j] == 0) {
-// 					Board.board[i][j] = player;
+	public static int move(int player, int depth) {
+		previousScore = evaluate(player);
+		Result bestScore = new Result(-1000000, depth+1);
+		int bestMove = -1;
 
-// 					if (Board.winCheck(player)) return (i+1)*10 + (j+1);
-// 					if (Board.loseCheck(player)) {
-// 						Board.board[i][j] = 0;
-// 						continue;
-// 					}
+		System.out.println("Eval: " + previousScore);
 
-// 					Move score = minimax(player, depth-1, new Move(-1000000, depth+1), new Move(1000000, depth+1), false);
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				if (Board.board[i][j] == 0) {
+					Board.board[i][j] = player;
 
-// 					Board.board[i][j] = 0;
+					if (Board.winCheck(player)) return (i+1)*10 + (j+1);
+					if (Board.loseCheck(player)) {
+						Board.board[i][j] = 0;
+						continue;
+					}
 
-// 					if (score.isBetter(bestScore, previousScore)) {
-// 						bestScore = score;
-// 						bestMove = (i+1)*10 + (j+1);
-// 					}
-// 				}
-// 			}
-// 		}
+					Result score = minimax(player, depth-1, new Result(-1000000, depth+1), new Result(1000000, depth+1), false);
 
-// 		System.out.println("Best move: " + bestScore.score + " on " + bestScore.depth);
+					Board.board[i][j] = 0;
 
-// 		// wszystkie ruchy prowadza do porazki
-// 		if (bestScore.score == -1000000) {
-// 			for (int i = 0; i < 5; i++) {
-// 				for (int j = 0; j < 5; j++) {
-// 					if (Board.board[i][j] == 0) return (i+1)*10 + (j+1);
-// 				}
-// 			}
-// 		}
+					if (score.isBetter(bestScore, previousScore)) {
+						bestScore = score;
+						bestMove = (i+1)*10 + (j+1);
+					}
+				}
+			}
+		}
 
-// 		// -1 gdy nie ma wolnych pol
-// 		return bestMove == -1 ? 0 : bestMove;
-// 	}
+		System.out.println("Best move: " + bestScore.score + " on " + bestScore.depth);
 
-// 	private static Move minimax(int player, int depth, Move best, Move worst, boolean maximizing) {
-// 		if (Board.winCheck(player)) return new Move(2000, depth);
-// 		if (Board.winCheck(3 - player)) return new Move(-2000, depth);
-// 		if (Board.loseCheck(player)) return new Move(-1000, depth);
-// 		if (Board.loseCheck(3 - player)) return new Move(1000, depth);
+		// wszystkie ruchy prowadza do porazki
+		if (bestScore.score == -1000000) {
+			for (int i = 0; i < 5; i++) {
+				for (int j = 0; j < 5; j++) {
+					if (Board.board[i][j] == 0) return (i+1)*10 + (j+1);
+				}
+			}
+		}
 
-// 		if (depth == 0) return new Move(evaluate(player), 0);
+		// -1 gdy nie ma wolnych pol
+		return bestMove == -1 ? 0 : bestMove;
+	}
 
-// 		Move bestScore = maximizing
-// 				? new Move(-1000000, depth + 1)
-// 				: new Move(1000000, depth + 1);
+	private static Result minimax(int player, int depth, Result alpha, Result beta, boolean maximizing) {
+		if (Board.winCheck(player)) return new Result(2000, depth); // pewna wygrana
+		if (Board.winCheck(3 - player)) return new Result(-2000, depth); // pewna porazka
+		if (Board.loseCheck(player)) return new Result(-1000, depth); // pewny brak wygranej
+		if (Board.loseCheck(3 - player)) return new Result(1000, depth); // pewny brak porazki
 
-// 		for (int i = 0; i < 5; i++) {
-// 			for (int j = 0; j < 5; j++) {
-// 				if (Board.board[i][j] == 0) {
-// 					int current = maximizing ? player : 3 - player;
-// 					Board.board[i][j] = current;
+		if (depth == 0) return new Result(evaluate(player), 0);
 
-// 					Move score = minimax(player, depth - 1, best, worst, !maximizing);
+		int opp = 3 - player;
 
-// 					Board.board[i][j] = 0;
-
-// 					if (maximizing) {
-// 						if (score.isBetter(bestScore, previousScore)) bestScore = score;
-// 						if (bestScore.isBetter(best, previousScore)) best = bestScore;
-// 						// if (best.isBetter(worst, previousScore)) break;
-// 						if (best.isBetter(worst, previousScore)) return bestScore;
-// 					} else {
-// 						if (bestScore.isBetter(score, previousScore)) bestScore = score;
-// 						if (worst.isBetter(bestScore, previousScore)) worst = bestScore;
-// 						// if (best.isBetter(worst, previousScore)) break;
-// 						if (best.isBetter(worst, previousScore)) return bestScore;
-// 					}
-// 				}
-// 			}
-// 			// if (best.isBetter(worst, previousScore)) break;
-// 		}
-// 		return bestScore;
-// 	}
-
-// 	// heurystyka
-// 	private static int evaluate(int player) {
-// 		int opp = 3 - player;
-
-// 		int m3 = 0, m2 = 0, availableWinLines = 0;
-// 		for (int[][] line : Board.win) {
-// 			int[] count = new int[3];
-// 			for (int i = 0; i < 4; i++) count[Board.board[line[i][0]][line[i][1]]]++;
-
-// 			// 3 zajęte i 1 puste w linii: max = 28
-// 			if (count[player] == 3 && count[0] == 1) m3++;
-// 			if (count[opp] == 3 && count[0] == 1) m3--;
-
-// 			// 2 zajęte i 2 puste w linii: max = 28
-// 			if (count[player] == 2 && count[0] == 2) m2++;
-// 			if (count[opp] == 2 && count[0] == 2) m2--;
-
-// 			// wolne linie wygrywające: max = 28
-// 			if (count[opp] == 0) availableWinLines++;
-// 			if (count[player] == 0) availableWinLines--;
-// 		}
+		// lista dostepnych ruchow, w pozniejszej fazie sortowana, aby alfa-beta ciecia byly bardziej efektywne
+		List<Move> moves = new ArrayList<>();
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				if (Board.board[i][j] == 0) {
+					Board.board[i][j] = maximizing ? player : opp;
+					moves.add(new Move(evaluate(player), i, j));
+					Board.board[i][j] = 0;
+				}
+			}
+		}
 		
-// 		// różnica dostępnych przegrywających linii: max = 48
-// 		int loseLineDiff = 0;
-// 		for (int[][] line : Board.lose) {
-// 			boolean canWinPlayer = true;
-// 			boolean canWinOpp = true;
-// 			for (int[] p : line) {
-// 				int val = Board.board[p[0]][p[1]];
-// 				if (val == opp) canWinPlayer = false;
-// 				if (val == player) canWinOpp = false;
-// 			}
-// 			if (canWinPlayer) loseLineDiff--;
-// 			if (canWinOpp) loseLineDiff++;
-// 		}
-		
-// 		// różnica dostępnych (nie powodujących natychmiastowej przegranej) ruchów: max = 25
-// 		int availableDiff = 0;
-// 		for (int i = 0; i < 5; i++) {
-// 			for (int j = 0; j < 5; j++) {
-// 				if (Board.board[i][j] == 0) {
-// 					Board.board[i][j] = player;
-// 					if (!Board.loseCheck(player)) availableDiff++;
-// 					Board.board[i][j] = 0;
-					
-// 					Board.board[i][j] = opp;
-// 					if (!Board.loseCheck(opp)) availableDiff--;
-// 					Board.board[i][j] = 0;
-// 				}
-// 			}
-// 		}
-		
-// 		int score = 0;
+		if (maximizing) {
+			moves.sort(Comparator.comparingInt(m -> -m.score));
 
-// 		score += 4*m3;
-// 		score += 2*m2;
-// 		score += 4*availableWinLines;
-// 		score += 2*loseLineDiff;
-// 		score += 4*availableDiff;
+			int n = moves.size();
+			for (int i = 0; i < n; i++) {
+				int c = moves.get(i).col, r = moves.get(i).row;
+				if (Board.board[c][r] == 0) {
+					Board.board[c][r] = player;
+					Result score = minimax(player, depth-1, alpha, beta, false);
+					Board.board[c][r] = 0;
 
-// 		return score;
-// 	}
-// }
+					if (score.score == moves.get(i).score) score.depth = depth;
+					if (score.isBetter(alpha, previousScore)) alpha = score;
+					if (alpha.isBetter(beta, previousScore)) return alpha;
+
+				}
+			}
+
+			return alpha;
+		} else {
+			moves.sort(Comparator.comparingInt(m -> m.score));
+			
+			int n = moves.size();
+			for (int i = 0; i < n; i++) {
+				int c = moves.get(i).col, r = moves.get(i).row;
+				if (Board.board[c][r] == 0) {
+					Board.board[c][r] = opp;
+					Result score = minimax(player, depth-1, alpha, beta, true);
+					Board.board[c][r] = 0;
+
+					if (score.score == moves.get(i).score) score.depth = depth;
+					if (beta.isBetter(score, previousScore)) beta = score;
+					if (alpha.isBetter(beta, previousScore)) return beta;
+				}
+			}
+
+			return beta;
+		}
+	}
+
+	private static boolean equal(int[] a, int[] b) {
+		for (int i = 0; i < 4; i++) 
+			if (a[i] != b[i]) return false;
+		return true;
+	}
+
+	// heurystyka
+	private static int evaluate(int player) {
+		int opp = 3 - player;
+		int bonus = 0;
+
+		int xoox = 0, oxxo = 0, three = 0, oxox = 0, xxoo = 0;
+
+		int n = Board.win.length;
+		for (int i = 0; i < n; i++) {
+			int[] line = new int[4];
+			int[] counter = new int[3];
+			for (int j = 0; j < 4; j++) {
+				line[j] = Board.board[Board.win[i][j][0]][Board.win[i][j][1]] == 0 ? 0 : 1;
+				counter[Board.board[Board.win[i][j][0]][Board.win[i][j][1]]]++;
+			}
+
+			if (equal(line, new int[]{1,0,0,1})) {
+				if (counter[player] > 0 && counter[opp] == 0) xoox++;
+				// else if (counter[opp] > 0 && counter[player] == 0) xoox--;
+				else if (counter[opp] > 0 && counter[player] == 0) {xoox--; bonus -= 200;}
+			} else if (equal(line, new int[]{0,1,1,0})) {
+				// if (counter[player] > 0 && counter[opp] == 0) oxxo--;
+				if (counter[player] > 0 && counter[opp] == 0) {oxxo--; bonus -= 100;}
+				else if (counter[opp] > 0 && counter[player] == 0) oxxo++;
+			} else if (counter[player] == 3 && counter[opp] == 0) three++;
+			// else if (counter[opp] == 3 && counter[player] == 0) three--;
+			else if (counter[opp] == 3 && counter[player] == 0) {three--; bonus -= 300;}
+			else if (equal(line, new int[]{1,0,1,0}) || equal(line, new int[]{1,0,1,0})) {
+				if (counter[player] > 0 && counter[opp] == 0) oxox++;
+				else if (counter[opp] > 0 && counter[player] == 0) oxox--;
+			} else if (equal(line, new int[]{1,1,0,0}) || equal(line, new int[]{0,0,1,1})) {
+				if (counter[player] > 0 && counter[opp] == 0) xxoo++;
+				else if (counter[opp] > 0 && counter[player] == 0) xxoo--;
+			}
+		}
+
+		int aval = 0;
+		n = Board.lose.length;
+		for (int i = 0; i < n; i++) {
+			int[] counter = new int[3];
+			for (int j = 0; j < 3; j++) counter[Board.board[Board.lose[i][j][0]][Board.lose[i][j][1]]]++;
+			
+			if (counter[opp] == 2 && counter[player] == 0) aval++; 
+			if (counter[player] == 2 && counter[opp] == 0) aval--;
+		}
+
+		int score = 0;
+		
+		score += 5*three;
+		score += 3*xoox;
+		score += 3*oxxo;
+		score += oxox;
+		score += xxoo;
+		score += 10*aval;
+
+		return score + bonus;
+	}
+}
