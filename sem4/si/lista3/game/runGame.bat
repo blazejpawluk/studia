4e9923c2abb049aeb731aa16d8357fe36b710ae1
Błażej Pawluk <bpawluk05@gmail.com>
2025-06-08 19:31:37 +0200

    si/l/l3 library and heuristic update

--- sem4/si/lista3/game/runGame.bat
+++ sem4/si/lista3/game/runGame.bat
@@ -9,17 +9,17 @@ start "Game Server" cmd /k ^
 @REM   "cd /d %~dp0 && game\game_client.exe 127.0.0.1 4444 1 client"
 
 @REM REM Losowy bot
-@REM start "Random Bot Client" cmd /k ^
-@REM   "cd /d %~dp0 && game\game_random_bot.exe 127.0.0.1 4444 2 client"
+@REM start "Random Bot" cmd /k ^
+@REM   "cd /d %~dp0 && game\game_random_bot.exe 127.0.0.1 4444 2 random"
 
 REM Bot
-start "Bot Client1" cmd /k ^
-  "cd /d %~dp0 && javac -d bin java\*.java && java -cp bin BotClient 127.0.0.1 4444 2 bot 7"
+start "Bot 1" cmd /k ^
+  "cd /d %~dp0 && javac -d bin java\*.java && java -cp bin BotClient 127.0.0.1 4444 1 bot 6"
 
 @REM REM Bot
-@REM start "Bot Client2" cmd /k ^
+@REM start "Bot 2" cmd /k ^
 @REM   "cd /d %~dp0 && javac -d bin java\*.java && java -cp bin BotClient 127.0.0.1 4444 2 bot 5"
 
 REM Dawid
 start "Bot Dawid" cmd /k ^
-  "cd /d %~dp0 && game\dawid3.exe 127.0.0.1 4444 1 dawid 7"
+  "cd /d %~dp0 && game\dawid.exe 127.0.0.1 4444 2 dawid 6"
